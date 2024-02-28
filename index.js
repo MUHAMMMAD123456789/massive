23556bee13decc61482eb1b7288c26916225ab8a
Ali <daminovmuhammadali3@gmail.com>
2024-02-28 18:09:44 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+// let users = ['Alex', 'John', 'Michael', 'Bob', 'Elza', 'Shmelza', 'Mikhail', 'Jago']
+
+
+// let M = users.filter((name) => name[0] === "M")
+
+// map - boolean
+// find - one
+// forEach - undefined
+
+
+
+
+// let users = ['Alex', 'John', 'Michael', 'Bob', 'Elza', 'Shmelza', 'Mikhail', 'Jago']
+
+
+// let evens = 0
+// let odds = 0
+
+// let count = users.filter((name) => { if (name.length % 2 === 0) { evens++ } else { odds++ } })
+
+// console.log(count);
+
+
+
+
+// let users = ['Alex', 'John', 'Michael', 'Bob', 'Elza', 'Shmelza', 'Mikhail', 'Jago']
+
+// let evens = users.filter((name) => (name.length % 2 === 0 )  ) 
+// let odds = users.filter((name) => (name.length % 2 !== 0 )  ) 
+
+
+// console.log(evens);
+// console.log(odds);
+
+
+
+
+
+
+let cars = [18000,15000,22000,33000,5000,30000,40000,37000,60000,40000]
+
+let from = prompt('от')
+
+let up = prompt('до')
+
+
+
+let priceOfcars = cars.filter( i => up => i => from );
+
+console.log(priceOfcars);
+
+
+
+
+
+
+
+
+
+
+
+
